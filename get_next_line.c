e0e88189375d32c87495c8b35a8f91f3acd563d9
Dimitri Paccagnini <dimitri.paccagnini@laposte.net>
2021-11-30 16:06:36 +0100

    30.11.2021

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: dpaccagn <dpaccagn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 15:40:03 by dpaccagn          #+#    #+#             */
-/*   Updated: 2021/11/30 15:40:21 by dpaccagn         ###   ########.fr       */
+/*   Updated: 2021/11/30 16:06:05 by dpaccagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
+	int		BUFFER_SIZE;
+	char	*buff;
 
+	BUFFER_SIZE = 10;
+	buff = malloc (sizeof (char) * BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
 }
