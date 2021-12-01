0ee6f14f79e6b6cc0e835f5f68a5df95fc4ae85e
Dimitri Paccagnini <dimitri.paccagnini@laposte.net>
2021-12-01 17:04:19 +0100

    Commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,31 @@
 /*   By: dpaccagn <dpaccagn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 15:40:03 by dpaccagn          #+#    #+#             */
-/*   Updated: 2021/12/01 16:29:54 by dpaccagn         ###   ########.fr       */
+/*   Updated: 2021/12/01 17:03:32 by dpaccagn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_line(char *s_buff, int ret)
+char	*get_line(char *s_buff)
 {
+	char	*line;
 	char	*tmp_buff;
 	int		i;
 
 	i = 0;
+	if (s_buff == 0)
+		return (NULL);
 	while (s_buff[i] != '\n' && s_buff[i] != 0)
-	{
-		if (s_buff[i] == '\n')
-		{
-			tmp_buff = ft_substr(s_buff, 0, (i + 1));
-			free(s_buff);
-			s_buff = tmp_buff;
-		}
-		else if (s_buff[i] == 0)
-		{
-			tmp_buff = ft_substr(s_buff, 0, (ft_strlen(s_buff) - i));
-			free (s_buff);
-			s_buff = tmp_buff;
-		}
-	}
+		i++;
+	if (s_buff[i] == '\n')
+		i++;
+	line = ft_substr(s_buff, 0, i);
+	tmp_buff = ft_substr(s_buff, i, ft_strlen(s_buff));
+	free(s_buff);
+	s_buff = tmp_buff;
+	return (line);
 }
-
-
 
 void	get_reader(int	fd, char *s_buff)
 {
@@ -62,7 +57,7 @@ char	*get_next_line(int fd)
 
 	if(!(ft_strchr(s_buff, '\n')))
 		get_reader(fd, s_buff);
-	get_line(s_buff, ret);
+	get_line(s_buff);
 }
 
 /*
